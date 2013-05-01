643ac983080691e97191ab0d2b870b9bc8d04503
Vilius Kraujutis <viliusk@gmail.com>
2013-05-02 00:37:03 +0300

    Some styling changes: Constants are capital meaningful view names Assigning objects to member variables only if they are needed Variable names in english

--- src/lt/vgtu/daytimeclient/ServerioParametrai.java
+++ src/lt/vgtu/daytimeclient/ServerioParametrai.java
@@ -2,6 +2,6 @@ package lt.vgtu.daytimeclient;
 
 //Serverio parametrai
 public interface ServerioParametrai {
-	public static final String Adresas = "nist1-ny2.ustiming.org";
-	public static final int Prievadas = 13;
+	public static final String SERVER_URL = "nist1-ny2.ustiming.org";
+	public static final int SERVER_PORT_NUMBER = 13;
 }
